98cb09439aebba2b2dc1cb04085335fed81d2637
tadeifelipe <felipe_santostadei@hotmail.com>
2020-11-17 19:42:35 -0300

    add connection db

--- src/main/java/com/tadeifelipe/mudi/controller/HomeControlller.java
+++ src/main/java/com/tadeifelipe/mudi/controller/HomeControlller.java
@@ -3,6 +3,10 @@ package com.tadeifelipe.mudi.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,16 +16,16 @@ import com.tadeifelipe.mudi.model.Pedido;
 @Controller
 public class HomeControlller {
 	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	
 	@GetMapping("/home")
 	public String home(Model model) {
+		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
 		
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Xiaomi Redmi Note 9");
-		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/61VmUpCs7PL._AC_SL1000_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/Xiaomi-Redmi-Note-128GB-4GB/dp/B088HJ3FCX?ref_=Oct_s9_apbd_otopr_hd_bw_bHjJLCl&pf_rd_r=E6MXP06N8J39J3JYSFQD&pf_rd_p=1da659a0-948a-52ba-a216-243433981446&pf_rd_s=merchandised-search-11&pf_rd_t=BROWSE&pf_rd_i=16243803011");
-		pedido.setDescricao("uma descrição qualquer");
+		List<Pedido>pedidos = query.getResultList();
 		
-		List<Pedido> pedidos = Arrays.asList(pedido,pedido,pedido);
 		model.addAttribute("pedidos",pedidos);
 		
 		return "home";
--- src/main/java/com/tadeifelipe/mudi/model/Pedido.java
+++ src/main/java/com/tadeifelipe/mudi/model/Pedido.java
@@ -3,7 +3,19 @@ package com.tadeifelipe.mudi.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Entity
 public class Pedido {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
 	private String nomeProduto;
 	private BigDecimal valorNegociado;
 	private LocalDate dataEntrega;
